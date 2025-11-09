594ddcd45fb0ab323e75dc5b5b4bc9d5bced1ebc
Purba-M <purbasha.sinha.moni@gmail.com>
2025-11-09 23:28:33 +0600

    Initial

---
+++ index.js
@@ -0,0 +1,45 @@
+const express=require('express')
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app=express()
+const port=process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//coonnection of uri
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.stkfkwf.mongodb.net/?appName=Cluster1`;
+
+
+app.get('/',(req,res)=>{
+        res.send('Book Haven API is running')})
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  }
+  finally{
+   
+
+  }
+}
+run().catch(console.dir);
+
+app.listen(port,(req,res)=>{
+    console.log(`Server is running on ${port}`)
+})
