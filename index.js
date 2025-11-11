fbb95cd98528d05fbab9907989d674eeda34b0f8
Purba-M <purbasha.sinha.moni@gmail.com>
2025-11-12 00:37:16 +0600

    env ignore

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+
+
+
 const express=require('express')
 const cors = require('cors');
+
+
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express()
@@ -14,9 +19,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.stkfkwf.mongodb.net/?appName=Cluster1`;
 
 
-app.get('/',(req,res)=>{
-        res.send('Book Haven API is running')})
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,7 +30,61 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const db = client.db('bookHavenDB');
+    const booksCollection = db.collection('books');
     // Send a ping to confirm a successful connection
+    
+    app.get('/',(req,res)=>{
+        res.send('Book Haven API is running')})
+
+    //add new book
+   app.post('/add-book',async(req,res)=>{
+    const newbook = { ...req.body, createdAt: new Date() };
+    const result = await booksCollection.insertOne(newbook);
+    res.send(result);
+
+   })
+
+ 
+app.get('/latest-books', async (req,res) => {
+  const books = await booksCollection
+    .find()
+    .sort({ _id:1}).limit(6).toArray();
+    res.send(books);
+});
+
+
+   app.get('/all-books', async (req,res)=>{
+  const books = await booksCollection.find().toArray();
+  res.send(books);
+});
+  const { ObjectId } = require('mongodb');
+app.get('/book-details/:id', async (req,res)=>{
+  const id = req.params.id;
+  const book = await booksCollection.findOne({ _id: new ObjectId(id) });
+  res.send(book);
+});
+app.put('/update-book/:id', async (req,res)=>{
+  const id = req.params.id;
+  const update = req.body;
+  const result = await booksCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: update }
+  );
+  res.send(result);
+});
+app.delete('/delete-book/:id', async (req,res)=>{
+  const id = req.params.id;
+  const result = await booksCollection.deleteOne({ _id: new ObjectId(id) });
+  res.send(result);
+});
+// client will call: /myBooks?email=user@example.com
+app.get('/myBooks', async (req,res)=>{
+  const email = req.query.email;
+  const books = await booksCollection.find({ userEmail: email }).toArray();
+  res.send(books);
+});
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
